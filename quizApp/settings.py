8e7f1b419431c0f1e827b7021dd9da166ce287fa
Junayed-shanto <junayedshanto54@gmail.com>
2025-02-21 23:21:36 +0600

    added a host ip in settings.py

--- quizApp/settings.py
+++ quizApp/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-0#(u#79x4o^h+)-%%d0=o&!s_e3rpxvakl&is844!#5gl!vm=l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['217.76.63.211', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['161.97.141.58', '217.76.63.211', 'localhost', '127.0.0.1', ]
 
 
 # Application definition
